bcd6db7d73a6ff403aaa7d7335621a8f16193c8a
Akash <akhvnd@gmail.com>
2022-07-16 12:24:11 +0530

    modified:employee present absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,9 +1,24 @@
 package com.EmployeeWages.java;
+
+import java.util.Random;
+
 public class EmployeeWages {
 
 	public static void main(String[] args) {
 		
 		System.out.println(" Welcome to Employee Wage Computation ");
+        Random random = new Random();
+		
+        int attendance = random.nextInt(2);
+        
+        if (attendance == 1 ) {
+        	
+        	System.out.println(" Employee is Present ");
+        	
+        } else {
+        	
+        	System.out.println(" Employee is Absent ");
+        }
 		
 	}
 }
