4db48b6c022463b0dd68ad482e493ab5d0ff4d07
Akash <akhvnd@gmail.com>
2022-07-16 12:34:45 +0530

    modified:employee daily wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -3,10 +3,14 @@ package com.EmployeeWages.java;
 import java.util.Random;
 
 public class EmployeeWages {
+	
+	int wagePerhour = 20;
+	int fullDayhour = 8;
 
 	public static void main(String[] args) {
 		
 		System.out.println(" Welcome to Employee Wage Computation ");
+		EmployeeWages obj = new EmployeeWages();
         Random random = new Random();
 		
         int attendance = random.nextInt(2);
@@ -14,12 +18,17 @@ public class EmployeeWages {
         if (attendance == 1 ) {
         	
         	System.out.println(" Employee is Present ");
+        	obj.employeeWage();
         	
         } else {
         	
         	System.out.println(" Employee is Absent ");
         }
 		
+	}
+	public void employeeWage() {
+		int dailyWage = wagePerhour * fullDayhour;
+		System.out.println(" Employee daily wage is : "+ dailyWage);
 	}
 }
 		
