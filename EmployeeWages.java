b098a6ef0ad6ccc7c3ae429d36f11b6bd1a1851f
Akash <akhvnd@gmail.com>
2022-07-16 13:01:34 +0530

    modified: add part time employee

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -6,6 +6,7 @@ public class EmployeeWages {
 	
 	int wagePerhour = 20;
 	int fullDayhour = 8;
+	int partTimehour = 4;
 
 	public static void main(String[] args) {
 		
@@ -13,23 +14,29 @@ public class EmployeeWages {
 		EmployeeWages obj = new EmployeeWages();
         Random random = new Random();
 		
-        int attendance = random.nextInt(2);
+        int attendance = random.nextInt(3);
         
         if (attendance == 1 ) {
         	
         	System.out.println(" Employee is Present ");
         	obj.employeeWage();
         	
-        } else {
-        	
-        	System.out.println(" Employee is Absent ");
-        }
-		
-	}
-	public void employeeWage() {
-		int dailyWage = wagePerhour * fullDayhour;
-		System.out.println(" Employee daily wage is : "+ dailyWage);
-	}
+          } else if(attendance == 2) {
+          	
+          	System.out.println(" Part time Employee ");
+          	System.out.println(" Part time hour : " +obj.partTimehour);
+          	
+          } else {
+          	
+          	System.out.println(" Employee is Absent ");
+          }
+  	}
+  	
+  	public void employeeWage() {
+  		int dailyWage = wagePerhour * fullDayhour;
+  		System.out.println(" Employee daily wage is : "+ dailyWage);
+  	}
+ 
 }
 		
 
